59956483ffb70fce2252d47875d65d3674eb7735
zulfahmidev <developer.zulfahmi@gmail.com>
2023-11-28 18:48:34 +0700

    perbaikan bug

--- config/config.js
+++ config/config.js
@@ -13,20 +13,38 @@ module.exports = {
     "password": DB_PASS,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+        "ssl": {
+            "require": true,
+            "rejectUnauthorized": false
+        }
+     },
   },
   "test": {
     "username": DB_USER,
     "password": DB_PASS,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+        "ssl": {
+            "require": true,
+            "rejectUnauthorized": false
+        }
+     },
   },
   "production": {
     "username": DB_USER,
     "password": DB_PASS,
     "database": DB_NAME,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+        "ssl": {
+            "require": true,
+            "rejectUnauthorized": false
+        }
+     },
   }
 }
